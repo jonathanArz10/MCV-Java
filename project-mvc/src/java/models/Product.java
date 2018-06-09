f3033e7f1c01b4c9ba7d8c476f1e033a9d124cda
jonathanArz10 <jonathan.arzatej10@gmail.com>
2018-06-09 00:33:36 -0500

    Add files via upload

---
+++ project-mvc/src/java/models/Product.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author jonat
+ */
+public class Product {
+    private String name;
+    private double price;
+    private String category;
+    private String description;
+
+    public Product(String name, double price, String category, String description) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void save() {
+        System.out.println("Datos guardados...");
+    }
+}
